3207cc8ced1f912fb0ef2672074a73d692f0e576
Joaquin Diaz <diazjoaquin1997@gmail.com>
2025-03-20 11:11:26 -0300

    cambios

---
+++ src/WisdomPetMedicine.Hospital.Api/IntegrationEvents/PetTransferredToHospitalIntegrationEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WisdomPetMedicine.Hospital.Api.IntegrationEvents
+{
+    public class PetTransferredToHospitalIntegrationEventHandler : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlaggedForAdoptionIntegrationEventHandler.cs
@@ -0,0 +1,68 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WisdomPetMedicine.Rescue.Api.Infrastructure;
+using WisdomPetMedicine.Rescue.Domain.Entities;
+using WisdomPetMedicine.Rescue.Domain.Repositories;
+using WisdomPetMedicine.Rescue.Domain.ValueObjects;
+
+namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
+{
+    public class PetFlaggedForAdoptionIntegrationEventHandler : BackgroundService
+    {
+        private readonly ILogger<PetFlaggedForAdoptionIntegrationEventHandler> _logger;
+        private readonly ServiceBusClient _serviceBusClient;
+        private readonly ServiceBusProcessor _serviceBusProcessor;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        public PetFlaggedForAdoptionIntegrationEventHandler(IConfiguration configuration, 
+            ILogger<PetFlaggedForAdoptionIntegrationEventHandler> logger,
+            IServiceScopeFactory serviceScopeFactory)
+        {
+            this._logger = logger;
+            this._serviceScopeFactory = serviceScopeFactory;
+
+            _serviceBusClient = new ServiceBusClient(configuration["ServiceBus:ConnectionString"]);
+            _serviceBusProcessor = _serviceBusClient.CreateProcessor(configuration["ServiceBus:TopicName"], configuration["ServiceBus:SubscriptionName"]);
+
+            _serviceBusProcessor.ProcessMessageAsync += Processor_ProcessMessageAsync;
+            _serviceBusProcessor.ProcessErrorAsync += Processor_ProcessErrorAsync;
+        }
+
+        private Task Processor_ProcessErrorAsync(ProcessErrorEventArgs args)
+        {
+            _logger?.LogError(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task Processor_ProcessMessageAsync(ProcessMessageEventArgs args)
+        {
+            var body = args.Message.Body.ToString();
+            var theEvent = JsonConvert.DeserializeObject<PetFlaggedForAdoptionIntegrationEvent>(body);
+            await args.CompleteMessageAsync(args.Message);
+
+            using var scope = _serviceScopeFactory.CreateScope();
+            var repo = scope.ServiceProvider.GetRequiredService<IRescueRepository>();
+            var dbContext = scope.ServiceProvider.GetRequiredService<RescueDbContext>();
+            dbContext.RescuedAnimalsMetadata.Add(theEvent);
+
+            var rescuedAnimal = new RescuedAnimal(RescuedAnimalId.Create(theEvent.Id));
+            await repo.AddRescuedAnimalAsync(rescuedAnimal);
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await _serviceBusProcessor.StartProcessingAsync(stoppingToken);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _serviceBusProcessor.StopProcessingAsync(cancellationToken);
+        }
+    }
+}
