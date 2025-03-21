83f4614562940cb980bea1cf805d29718f2e2b15
Joaquin Diaz <diazjoaquin1997@gmail.com>
2025-03-21 15:41:23 -0300

    Commad and query implemented

---
+++ src/WisdomPetMedicine.Pet.Api/Commands/SetDateOfBirthCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WisdomPetMedicine.Pet.Api.Commands
+{
+    public class SetDateOfBirthCommand
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
